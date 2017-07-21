f06be7d57c605f2940185a9eec771c5929b0a35b
Jan Dahlke <jan.dahlke@pdfforge.org>
2017-07-21 08:23:11 +0200

    getting rid of old foo

--- src/containers/App/index.tsx
+++ src/containers/App/index.tsx
@@ -1,16 +1,13 @@
 import * as React from 'react';
-import * as TodoActions from '../../actions/todos';
-import * as style from './style.css';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { RouteComponentProps } from 'react-router';
 import { RootState } from '../../reducers';
-import { Header, MainSection } from '../../components';
+
 
 export namespace App {
   export interface Props extends RouteComponentProps<void> {
-    todos: TodoItemData[];
-    actions: typeof TodoActions;
+    //
   }
 
   export interface State {
@@ -22,25 +19,20 @@ export namespace App {
 export class App extends React.Component<App.Props, App.State> {
 
   render() {
-    const { todos, actions, children } = this.props;
-    return (
-      <div className={style.normal}>
-        <Header addTodo={actions.addTodo} />
-        <MainSection todos={todos} actions={actions} />
-        {children}
-      </div>
-    );
+      return (
+          <div>im not empty</div>
+      );
   }
 }
 
 function mapStateToProps(state: RootState) {
   return {
-    todos: state.todos
+    //
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    actions: bindActionCreators(TodoActions as any, dispatch)
+    //
   };
 }
